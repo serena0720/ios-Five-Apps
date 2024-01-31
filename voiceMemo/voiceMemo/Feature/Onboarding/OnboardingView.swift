eabb1ea13e9fb956d39795d9ba52e9161baaf5d8
Serena <sha0720@naver.com>
2024-01-31 11:08:31 +0900

    feat: OnboardingView의 subView 생성
    온보딩 컨텐츠 뷰

--- voiceMemo/voiceMemo/Feature/Onboarding/OnboardingView.swift
+++ voiceMemo/voiceMemo/Feature/Onboarding/OnboardingView.swift
@@ -6,13 +6,30 @@
 import SwiftUI
 
 struct OnboardingView: View {
-  var body: some View {
-    Text("Onboarding")
-  }
+    @StateObject private var onboardingViewModel = OnboardingViewModel()
+    
+    var body: some View {
+        //TODO: - 화면 전환 구현 필요
+        OnboardingContentView(onboardingViewModel: onboardingViewModel)
+    }
+}
+
+// MARK: - 온보딩 컨텐츠 뷰
+private struct OnboardingContentView: View {
+    @ObservedObject private var onboardingViewModel: OnboardingViewModel
+    
+    fileprivate init(onboardingViewModel: OnboardingViewModel) {
+        self.onboardingViewModel = onboardingViewModel
+    }
+    fileprivate var body: some View {
+        VStack {
+            Text("123")
+        }
+    }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
-  static var previews: some View {
-    OnboardingView()
-  }
+    static var previews: some View {
+        OnboardingView()
+    }
 }
