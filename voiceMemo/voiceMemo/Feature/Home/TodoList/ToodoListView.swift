83189a5bf7d5ac08f2219bcd6db23b4cee494290
Serena <sha0720@naver.com>
2024-01-31 20:03:02 +0900

    feat: TodoList의 TitleView 생성

--- voiceMemo/voiceMemo/Feature/Home/TodoList/ToodoListView.swift
+++ voiceMemo/voiceMemo/Feature/Home/TodoList/ToodoListView.swift
@@ -8,6 +8,46 @@ import SwiftUI
 struct TodoListView: View {
     @EnvironmentObject private var pathModel: PathModel
     @EnvironmentObject private var todoListViewModel: TodoListViewModel
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                if !todoListViewModel.todos.isEmpty {
+                    CustomNavigationBar(
+                        isDisplayLeftBtn: false,
+                        rightBtnAction: {
+                            todoListViewModel.navigationRightBtnTapped()
+                        },
+                        rightBtnType: todoListViewModel.navigationBarRightBtnMode
+                    )
+                } else {
+                    Spacer()
+                        .frame(height: 30)
+                }
+                TitleView()
+            }
+        }
+    }
+}
+
+// MARK: - TodoList 타이틀 뷰
+private struct TitleView: View {
+    @EnvironmentObject private var todoListViewModel: TodoListViewModel
+    
+    fileprivate var body: some View {
+        HStack {
+            if todoListViewModel.todos.isEmpty {
+                Text("To do List를\n추가해 보세요.")
+            } else {
+                Text("To do List \(todoListViewModel.todos.count)개가\n있습니다.")
+            }
+            
+            Spacer()
+        }
+        .font(.system(size: 30, weight: .bold))
+        .padding(.leading, 20)
+    }
+}
 }
 
 struct TodoListView_Previews: PreviewProvider {
