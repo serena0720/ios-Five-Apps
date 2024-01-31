e232a8ea19b9db37cc34d57f585cca8e6258b563
Serena <sha0720@naver.com>
2024-01-31 11:42:37 +0900

    feat: OnboardingCellListView 생성 및 적용

--- voiceMemo/voiceMemo/Feature/Onboarding/OnboardingView.swift
+++ voiceMemo/voiceMemo/Feature/Onboarding/OnboardingView.swift
@@ -23,8 +23,39 @@ private struct OnboardingContentView: View {
     }
     fileprivate var body: some View {
         VStack {
-            Text("123")
+            OnboardingCellListView(onboardingViewModel: onboardingViewModel)
         }
+    }
+}
+
+// MARK: - 온보딩 셀 리스트 뷰
+private struct OnboardingCellListView: View {
+    @ObservedObject private var onboardingViewModel: OnboardingViewModel
+    @State private var selectedIndex: Int
+    
+    fileprivate init(
+        onboardingViewModel: OnboardingViewModel,
+        selectedIndex: Int = 0
+    ) {
+        self.onboardingViewModel = onboardingViewModel
+        self.selectedIndex = selectedIndex
+    }
+    
+    fileprivate var body: some View {
+        TabView(selection: $selectedIndex) {
+            ForEach(Array(onboardingViewModel.onboardingContents.enumerated()), id: \.element) { index, onboardingContent in
+                OnboardingCellView(onboardingContent: onboardingContent)
+                    .tag(index)
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 1.5)
+        .background(
+            selectedIndex % 2 == 0
+            ? Color.customSky
+            : Color.customBackgroundGreen
+        )
+        .clipped()
     }
 }
 
