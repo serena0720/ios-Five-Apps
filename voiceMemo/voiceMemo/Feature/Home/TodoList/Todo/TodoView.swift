880cdb3ad7c195b257a06e94ae454ad25f9a5146
Serena <sha0720@naver.com>
2024-01-31 21:57:40 +0900

    feat: TodoView 생성 및 프로퍼티 생성

--- voiceMemo/voiceMemo/Feature/Home/TodoList/Todo/TodoView.swift
+++ voiceMemo/voiceMemo/Feature/Home/TodoList/Todo/TodoView.swift
@@ -6,13 +6,36 @@
 import SwiftUI
 
 struct TodoView: View {
-  var body: some View {
-    Text("Todo")
-  }
+    @EnvironmentObject private var pathModel: PathModel
+    @EnvironmentObject private var todoListViewModel: TodoListViewModel
+    @StateObject private var todoViewModel = TodoViewModel()
+    
+    var body: some View {
+        VStack {
+            CustomNavigationBar(
+                leftBtnAction: {
+                    pathModel.paths.removeLast()
+                },
+                rightBtnAction: {
+                    todoListViewModel.addTodo(
+                        .init(
+                            title: todoViewModel.title,
+                            time: todoViewModel.time,
+                            day: todoViewModel.day,
+                            selected: false
+                        )
+                    )
+                    pathModel.paths.removeLast()
+                },
+                rightBtnType: .create
+            )
+        }
+    }
+}
 }
 
 struct TodoView_Previews: PreviewProvider {
-  static var previews: some View {
-    TodoView()
-  }
+    static var previews: some View {
+        TodoView()
+    }
 }
