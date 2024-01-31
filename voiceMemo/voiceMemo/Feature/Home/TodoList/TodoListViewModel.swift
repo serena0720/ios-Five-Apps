ce96e3d22c4d592f02010dd9aa8926871b5ce6ec
Serena <sha0720@naver.com>
2024-01-31 19:14:17 +0900

    feat: TodoListViewModel 프로퍼티 생성

--- voiceMemo/voiceMemo/Feature/Home/TodoList/TodoListViewModel.swift
+++ voiceMemo/voiceMemo/Feature/Home/TodoList/TodoListViewModel.swift
@@ -5,6 +5,29 @@
 
 import Foundation
 
-class TodoListViewModel {
-  
+class TodoListViewModel: ObservableObject {
+    @Published var todos: [Todo]
+    @Published var isEditTodoMode: Bool
+    @Published var removeTodos: [Todo]
+    @Published var isDisplayRemoveTodoAlert: Bool
+    
+    var removeTodosCount: Int {
+        return removeTodos.count
+    }
+    var navigationBarRightBtnMode: NavigationBtnType {
+        isEditTodoMode ? .complete : .edit
+    }
+    
+    init(
+        todos: [Todo] = [],
+        isEditModeTodoMode: Bool = false,
+        removeTodos: [Todo] = [],
+        isDisplayRemoveTodoAlert: Bool = false
+    ) {
+        self.todos = todos
+        self.isEditTodoMode = isEditModeTodoMode
+        self.removeTodos = removeTodos
+        self.isDisplayRemoveTodoAlert = isDisplayRemoveTodoAlert
+    }
+}
 }
